023d2b54eb80613db07c599f706b1846dd75343e
onsdagens <pawel.dzialo@ltu.se>
2025-03-18 17:48:15 +0100

    split text section also

--- src/main.rs
+++ src/main.rs
@@ -48,15 +48,27 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut ro_path: Vec<PathBuf> = vec![];
     let mut ro_f = vec![];
+
+    let mut text_path: Vec<PathBuf> = vec![];
+    let mut text_f = vec![];
     for i in 0..4 {
-        let mut p = if let Some(out_dir) = cli.out_dir.clone() {
+        let mut path = if let Some(out_dir) = cli.out_dir.clone() {
             out_dir
         } else {
             PathBuf::new()
         };
-        p.push(p.with_file_name(format!("data_{}.mem", i)));
-        ro_path.push(p.clone());
-        ro_f.push(fs::File::create(p)?);
+        path.push(path.with_file_name(format!("data_{}.mem", i)));
+        ro_path.push(path.clone());
+        ro_f.push(fs::File::create(path)?);
+
+        let mut path = if let Some(out_dir) = cli.out_dir.clone() {
+            out_dir
+        } else {
+            PathBuf::new()
+        };
+        path.push(path.with_file_name(format!("text_{}.mem", i)));
+        text_path.push(path.clone());
+        text_f.push(fs::File::create(path)?);
     }
     println!("Generate output:");
     println!("In elf file       : {:?}", cli.file);
@@ -67,24 +79,35 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let in_path = cli.file;
     let file_data = fs::read(in_path.clone())?;
-    let mut f_out_text = fs::File::create(out_path_text.clone())?;
 
     let data = file_data.as_slice();
     let elf = ElfFile::new(data)?;
 
     let text_section = elf.find_section_by_name(".text").unwrap();
-    dump_section(
-        &elf,
-        text_section,
-        data,
-        cli.width,
-        cli.packed,
-        &mut f_out_text,
-        !cli.native,
-    )?;
+    dump_text(&elf, text_section, data, &mut text_f)?;
 
     let data_section = elf.find_section_by_name(".rodata").unwrap();
     dump_ro_data(&elf, data_section, data, &mut ro_f)?;
+
+    Ok(())
+}
+
+fn dump_text(
+    elf: &ElfFile,
+    sh: SectionHeader,
+    data: &[u8],
+    text_f: &mut [fs::File],
+) -> Result<(), Box<dyn Error>> {
+    for (i, mut f) in text_f.iter().enumerate() {
+        writeln!(f, "// section {:?}[{}]", sh.get_name(elf)?, i)?;
+        writeln!(f, "@{:x?}", sh.address())?;
+    }
+
+    let d = &data[sh.offset() as usize..(sh.offset() + sh.size()) as usize];
+
+    for (i, byte) in d.iter().enumerate() {
+        write!(text_f[i % 4], "{:x} ", byte)?;
+    }
 
     Ok(())
 }
